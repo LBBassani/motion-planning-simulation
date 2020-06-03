ee2a1f6b60ceda5db934d36c1ed4d5b64052cac8
LBBassani <lorenabassani12@gmail.com>
2020-06-03 13:19:07 -0300

    utilizando gtkgui

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 
 
-from srimulatorcore.rimulator import Rimulator
+from srimulatorcore.rimulators_samples.rimulator import Rimulator
 from srimulatorcore.sobots_samples.kheperaiii.kheperaiii import Kheperaiii
-import threading
+from srimulatorgtk.window import SobotRimulatorWindow
 
 rimulator = Rimulator([(Kheperaiii, (0.5, 0.3)), (Kheperaiii, (-0.5, -0.3)) ])
 rimulator.add_robot((Kheperaiii, (0,0)))
-rimulator.start_sobot_rimulator()
+window = SobotRimulatorWindow(rimulator)
+window.start_sobot_rimulator()
